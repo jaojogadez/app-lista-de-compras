ce7bc736f324c73ba4777ad62a8edf581526fd6d
João Pedro de Oliveira <jao07oliveiragames@gmail.com>
2024-09-12 08:48:37 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const box = document.getElementById("item-box") // div onde fica os produtos
 const form = document.querySelector("form")
 const input = document.getElementById("item")
+const p = document.querySelector(".text-muted")
 let inputNumberID = 0
+let qtndListItem = 0
 
 form.onsubmit = (e) => {
     e.preventDefault()
@@ -9,6 +11,12 @@ form.onsubmit = (e) => {
     // cria uma 'row'
     const newRow = document.createElement("div") 
     newRow.classList.add("row", "p-3", "m-3", "shadow", "rounded-3")
+    qtndListItem++
+    
+    if(qtndListItem != 0){
+        p.classList.add("d-none")
+    }
+    
 
     // cria uma 'col'
     const newCol = document.createElement("div")
@@ -19,12 +27,30 @@ form.onsubmit = (e) => {
     newCol1.classList.add("col-1")
     const newTrash = document.createElement("i")
     newTrash.classList.add("bi", "bi-trash", "secondary-color", "fs-5")
+    newTrash.onclick = () =>{
+        newRow.remove()
+        qtndListItem--
+        if(qtndListItem === 0){
+            p.classList.remove("d-none")
+        }
+    }
 
     // cria input
     const newInput = document.createElement("input")
     newInput.setAttribute("type" ,"checkbox")
     newInput.id = `list-item${++inputNumberID}`
     newInput.classList.add("form-check-input", "rounded-circle", "form-control-md")
+    newInput.addEventListener("change", () => {
+        if(newInput.checked){
+            newRow.classList.add("checked")
+            newLabel.classList.add("text-decoration-line-through")
+        }
+        else{
+            newRow.classList.remove("checked")
+            newLabel.classList.remove("text-decoration-line-through")
+
+        }
+    })
 
     // cria label
     const newLabel = document.createElement("label")
@@ -42,5 +68,7 @@ form.onsubmit = (e) => {
     newRow.appendChild(newCol1)
 
     box.appendChild(newRow)
+    input.value = ""
+    console.log(qtndListItem)
 }
 
